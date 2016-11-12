f5f2cbd0046eed8c288e8d4d3e4cf1e1552bfecc
KumarArul <arulkumar.v@technoduce.com>
2016-11-12 12:18:13 +0530

    upload

---
+++ mylibrary/src/main/java/library/com/mylibrary/MyView.java
@@ -0,0 +1,26 @@
+package library.com.mylibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by tech on 11/8/2016.
+ */
+public class MyView extends LinearLayout {
+
+    public MyView(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    private void initialize(Context context){
+        inflate(context, R.layout.my_view, this);
+    }
+
+}
